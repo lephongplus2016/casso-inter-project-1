d8a7725405e190339168dba7983927bd290a9a28
lephongplus2016 <lephongplus2016@gmail.com>
2021-07-29 21:53:37 +0700

    add feature input start day

--- src/main.js
+++ src/main.js
@@ -1,101 +1,113 @@
 //hàm tạo Menu trên thanh công cụ ============================================================================================
-function onOpen(){
-  var ui = SpreadsheetApp.getUi();
-  ui.createMenu('Menu')
-  .addItem('Form Sheet', 'formSheet')
-  .addSeparator()
-  .addItem('Get User Info', 'runUserInfo')
-  .addSeparator()
-  .addItem('Get Transactions','runTransactions')
-  .addToUi();
+function onOpen() {
+    var ui = SpreadsheetApp.getUi();
+    ui.createMenu("Menu")
+        .addItem("Form Sheet", "formSheet")
+        .addSeparator()
+        .addItem("Get User Info", "runUserInfo")
+        .addSeparator()
+        .addItem("Get Transactions", "runTransactions")
+        .addToUi();
 }
 //============================================================================================================================
 
 // hàm lấy access token
 function postApiKeyToToken() {
-  var myFile = SpreadsheetApp.getActiveSpreadsheet();
-  var apiSheet = myFile.getSheetByName('Values of API');
-  var api_key = apiSheet.getRange('B1').getValue();
-  var data = {
-      code: api_key,
-  };
-  //Logger.log(api_key);
-  var options = {
-      method: "post",
-      contentType: "application/json",
-      // Convert the JavaScript object to a JSON string.
-      payload: JSON.stringify(data),
-  };
-  var response = UrlFetchApp.fetch(
-      "http://dev.casso.vn:3338/v1/token",
-      options
-  );
+    var myFile = SpreadsheetApp.getActiveSpreadsheet();
+    var apiSheet = myFile.getSheetByName("Values of API");
+    var api_key = apiSheet.getRange("B1").getValue();
+    var data = {
+        code: api_key,
+    };
+    //Logger.log(api_key);
+    var options = {
+        method: "post",
+        contentType: "application/json",
+        // Convert the JavaScript object to a JSON string.
+        payload: JSON.stringify(data),
+    };
+    var response = UrlFetchApp.fetch(
+        "http://dev.casso.vn:3338/v1/token",
+        options
+    );
 
     // là các giá trị mà  The HTTP response về
-  //Logger.log(response);
+    //Logger.log(response);
 
-  // convert về json object để sử dụng
-  var res = JSON.parse(response.getContentText());
-  //Logger.log(res.access_token);
+    // convert về json object để sử dụng
+    var res = JSON.parse(response.getContentText());
+    //Logger.log(res.access_token);
 
-  if(response != null){
-    apiSheet.getRange('A2').setValue('Refresh Token');
-    apiSheet.getRange('B2').setValue(res.refresh_token);
-    apiSheet.getRange('A3').setValue('Access Token');
-    apiSheet.getRange('B3').setValue(res.access_token);
-  }
-  else{
-    SpreadsheetApp.getUi().alert('Cannot get reponse from API');
-  }
+    if (response != null) {
+        apiSheet.getRange("A2").setValue("Refresh Token");
+        apiSheet.getRange("B2").setValue(res.refresh_token);
+        apiSheet.getRange("A3").setValue("Access Token");
+        apiSheet.getRange("B3").setValue(res.access_token);
+    } else {
+        SpreadsheetApp.getUi().alert("Cannot get reponse from API");
+    }
 
-  return res.access_token;
+    return res.access_token;
+}
+
+function convertDate(date) {
+    //xử lý ngày cho Transaction
+    var year = +date.substring(6, 10);
+    var month = +date.substring(3, 5);
+    var day = +date.substring(0, 2);
+    let date_trans = new Date(year, month - 1, day + 2);
+    date_trans = Utilities.formatDate(date_trans, "GTM", "yyyy-MM-dd");
+    return date_trans;
 }
 
 // cac ham trong menu ========================================================================================================
 function runUserInfo() {
-  var myFile = SpreadsheetApp.getActiveSpreadsheet();
-  var apiSheet = myFile.getSheetByName('Values of API');
-  var api_key = apiSheet.getRange('B1').getValue();
-  if(api_key == 'Fill API key here'){
-    SpreadsheetApp.getUi().alert('Please fill API Key!');
-  }
-  else {
-  showLoadingDialog();
-  var token = postApiKeyToToken();
-    getUserInfo(token);
-  }
-   
+    var myFile = SpreadsheetApp.getActiveSpreadsheet();
+    var apiSheet = myFile.getSheetByName("Values of API");
+    var api_key = apiSheet.getRange("B1").getValue();
+    if (api_key == "Fill API key here") {
+        SpreadsheetApp.getUi().alert("Please fill API Key!");
+    } else {
+        showLoadingDialog();
+        var token = postApiKeyToToken();
+        getUserInfo(token);
+    }
 }
 
-function runTransactions(){
-  var myFile = SpreadsheetApp.getActiveSpreadsheet();
-  var apiSheet = myFile.getSheetByName('Values of API');
-  var api_key = apiSheet.getRange('B1').getValue();
-  if(api_key == 'Fill API key here'){
-    SpreadsheetApp.getUi().alert('Please fill API Key!');
-  }
-  else {
-  showLoadingSlowDialog();
-  var token = postApiKeyToToken();
-    getTransaction(token);
-  }
+function runTransactions() {
+    var myFile = SpreadsheetApp.getActiveSpreadsheet();
+    var apiSheet = myFile.getSheetByName("Values of API");
+    var api_key = apiSheet.getRange("B1").getValue();
+    if (api_key == "Fill API key here") {
+        SpreadsheetApp.getUi().alert("Please fill API Key!");
+    } else {
+        // nhap ngay bat dau giao dich
+        var ui = SpreadsheetApp.getUi();
+        var res = ui.prompt(`Bạn muốn lấy danh sách giao dịch từ ngày nào?
+  ví dụ: 01-12-2021`);
+        var fromDate = res.getResponseText();
+
+        fromDate = convertDate(fromDate);
+        showLoadingSlowDialog();
+        var token = postApiKeyToToken();
+        getTransaction(fromDate, token);
+    }
 }
 //==========================================================================================================================
 
-
 // hieu ung ================================================================================================================
 function showLoadingDialog() {
-  var html = HtmlService.createHtmlOutputFromFile('loading')
-      .setWidth(200)
-      .setHeight(100);
-  SpreadsheetApp.getUi() // Or DocumentApp or SlidesApp or FormApp.
-      .showModalDialog(html, 'App is loading!');
+    var html = HtmlService.createHtmlOutputFromFile("loading")
+        .setWidth(200)
+        .setHeight(100);
+    SpreadsheetApp.getUi() // Or DocumentApp or SlidesApp or FormApp.
+        .showModalDialog(html, "App is loading!");
 }
 
 function showLoadingSlowDialog() {
-  var html = HtmlService.createHtmlOutputFromFile('loadingSlow')
-      .setWidth(200)
-      .setHeight(100);
-  SpreadsheetApp.getUi() // Or DocumentApp or SlidesApp or FormApp.
-      .showModalDialog(html, 'App is loading!');
+    var html = HtmlService.createHtmlOutputFromFile("loadingSlow")
+        .setWidth(200)
+        .setHeight(100);
+    SpreadsheetApp.getUi() // Or DocumentApp or SlidesApp or FormApp.
+        .showModalDialog(html, "App is loading!");
 }
--- src/transactions.js
+++ src/transactions.js
@@ -1,48 +1,50 @@
 // Hàm api lấy transactions
-function getTransaction(token){
-  var options = {
-      method: "get",
-      headers: {
-          Authorization: token,
-      },
-  };
-  var response = UrlFetchApp.fetch(
-      "http://dev.casso.vn:3338/v1/transactions?fromDate=2021-07-01&sort=DESC&pageSize=100",
-      options
-  );
-  var res = JSON.parse(response.getContentText());
-  Logger.log(res.data.records);
-  addTransactions(res.data.records);
+function getTransaction(fromDate, token) {
+    var options = {
+        method: "get",
+        headers: {
+            Authorization: token,
+        },
+    };
+    var response = UrlFetchApp.fetch(
+        "http://dev.casso.vn:3338/v1/transactions?fromDate=" +
+            fromDate +
+            "&sort=DESC&pageSize=100",
+        options
+    );
+    var res = JSON.parse(response.getContentText());
+    Logger.log(res.data.records);
+    addTransactions(res.data.records);
 }
 
 // them transactions vao sheet 'Transactions'
-function addTransactions(data){
-  
-  transactionSheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('Transactions');
-  data.forEach( function(i) {
-    let id = i.id;
-    let date = i.when;
+function addTransactions(data) {
+    transactionSheet =
+        SpreadsheetApp.getActiveSpreadsheet().getSheetByName("Transactions");
+    data.forEach(function (i) {
+        let id = i.id;
+        let date = i.when;
 
-    //xử lý ngày
-    var year = +date.substring(0, 4);
-    var month = +date.substring(5, 7);
-    var day = +date.substring(8, 10);
-    let date_trans = new Date(year, month - 1, day);
-    date_trans = Utilities.formatDate(date_trans, "GTM", "dd-MM-yyyy");
+        //xử lý ngày
+        var year = +date.substring(0, 4);
+        var month = +date.substring(5, 7);
+        var day = +date.substring(8, 10);
+        let date_trans = new Date(year, month - 1, day);
+        date = Utilities.formatDate(date_trans, "GTM", "dd-MM-yyyy");
 
-    //xử lý description
-    let des = i.description;
-    if(des.length > 50){
-      var str1 = des.substr(0, des.length/2);
-      var str2 = des.substr(des.length/2);
-      str1 = str1 + str2.substr(0, str2.indexOf(' '));
-      str2 = str2.substr(str2.indexOf(' '));
-      des = str1 + "\n" + str2;
-    }
-    
-    let bankId = i.bankSubAccId;
-    let amount = i.amount;
-    var row = [id, date_trans, des, bankId, amount];
-    transactionSheet.appendRow(row);
-  } )
+        //xử lý description
+        let des = i.description;
+        if (des.length > 50) {
+            var str1 = des.substr(0, des.length / 2);
+            var str2 = des.substr(des.length / 2);
+            str1 = str1 + str2.substr(0, str2.indexOf(" "));
+            str2 = str2.substr(str2.indexOf(" "));
+            des = str1 + "\n" + str2;
+        }
+
+        let bankId = i.bankSubAccId;
+        let amount = i.amount;
+        var row = [id, date, des, bankId, amount];
+        transactionSheet.appendRow(row);
+    });
 }
