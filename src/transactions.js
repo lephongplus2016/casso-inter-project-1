a59c7fd51635e769cfae4bcbda72348469336eac
TranYenNhibk <67133814+TranYenNhibk@users.noreply.github.com>
2021-07-29 21:25:32 +0700

    Update handle date and description

--- src/transactions.js
+++ src/transactions.js
@@ -22,10 +22,27 @@ function addTransactions(data){
   data.forEach( function(i) {
     let id = i.id;
     let date = i.when;
+
+    //xử lý ngày
+    var year = +date.substring(0, 4);
+    var month = +date.substring(5, 7);
+    var day = +date.substring(8, 10);
+    let date_trans = new Date(year, month - 1, day);
+    date_trans = Utilities.formatDate(date_trans, "GTM", "dd-MM-yyyy");
+
+    //xử lý description
     let des = i.description;
+    if(des.length > 50){
+      var str1 = des.substr(0, des.length/2);
+      var str2 = des.substr(des.length/2);
+      str1 = str1 + str2.substr(0, str2.indexOf(' '));
+      str2 = str2.substr(str2.indexOf(' '));
+      des = str1 + "\n" + str2;
+    }
+    
     let bankId = i.bankSubAccId;
     let amount = i.amount;
-    var row = [id, date, des, bankId, amount];
+    var row = [id, date_trans, des, bankId, amount];
     transactionSheet.appendRow(row);
   } )
 }
